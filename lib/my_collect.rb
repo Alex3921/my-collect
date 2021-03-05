e89c4a39905213448b42558e8ca4536423df0fe8
alexandru <boncut.alex10@gmail.com>
2021-03-05 16:06:43 -0600

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(array)
+  i = 0
+  array_final = []
 
+  if array.length == 0
+  end
+
+  while i < array.length
+    array_final << yield(array[i])
+    i += 1    
+  end
+  array_final
+end
+
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end 
 
